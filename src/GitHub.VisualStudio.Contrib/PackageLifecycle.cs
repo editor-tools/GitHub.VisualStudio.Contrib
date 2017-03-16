243bf4daf1197e9580552caa931811157451fa0b
Jamie Cansdale <jcansdale@gmail.com>
2017-03-16 10:39:10 +0000

    Consolidate into single GitHubCommand class
    Use MEF to track object lifecycles.

--- src/GitHub.VisualStudio.Contrib/PackageLifecycle.cs
+++ src/GitHub.VisualStudio.Contrib/PackageLifecycle.cs
@@ -2,19 +2,56 @@
 {
     using System;
     using Microsoft.VisualStudio.Shell;
+    using EnvDTE80;
+    using System.IO;
+    using System.Reflection;
+    using System.Diagnostics;
+    using System.ComponentModel.Composition;
+    using System.ComponentModel.Composition.Hosting;
+    using GitHub.Services;
+    using EnvDTE;
 
     public class PackageLifecycle : IDisposable
     {
-        GitHubCommand gitHubCommand;
+        Package package;
+        CompositionContainer container;
 
         public PackageLifecycle(Package package)
         {
-            gitHubCommand = new GitHubCommand(package);
+            this.package = package;
+            var dte = (DTE2)ServiceProvider.GetService(typeof(DTE));
+            try
+            {
+                Init(ServiceProvider);
+            }
+            catch (FileNotFoundException e) when (e.FileName.StartsWith("GitHub.Exports,"))
+            {
+                var assemblyName = new AssemblyName(e.FileName);
+                var message = $"Please install GitHub for Visual Studio version {assemblyName.Version} or later";
+                dte.StatusBar.Text = message;
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e);
+                dte.StatusBar.Text = e.ToString();
+            }
+        }
+
+        IServiceProvider ServiceProvider => package;
+
+        void Init(IServiceProvider serviceProvider)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyCatalog = new AssemblyCatalog(assembly);
+            var sp = (IGitHubServiceProvider)serviceProvider.GetService(typeof(IGitHubServiceProvider));
+            container = new CompositionContainer(assemblyCatalog, sp.ExportProvider);
+            container.ComposeExportedValue(package);
+            container.GetExportedValue<GitHubCommand>();
         }
 
         public void Dispose()
         {
-            gitHubCommand.Dispose();
+            container.Dispose();
         }
     }
 }
