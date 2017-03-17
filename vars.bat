b44d1139c6d6242088af6f7d1d8bf07cbf24c9e3
Jamie Cansdale <jcansdale@gmail.com>
2017-03-17 15:27:28 +0000

    Make it work with all VS 2017 SKUs

--- vars.bat
+++ vars.bat
@@ -7,10 +7,13 @@ rem Utility for re/installing VSIX
 set path=%cd%\tools\VsixUtil;%path%
 
 rem Which SKU to use
-set VisualStudioSKU=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise
+set VisualStudioVersionPath=%ProgramFiles(x86)%\Microsoft Visual Studio\2017
+if "%VisualStudioSKUPath%" == "" if exist "%VisualStudioVersionPath%\Enterprise" set VisualStudioSKUPath=%VisualStudioVersionPath%\Enterprise
+if "%VisualStudioSKUPath%" == "" if exist "%VisualStudioVersionPath%\Professional" set VisualStudioSKUPath=%VisualStudioVersionPath%\Professional
+if "%VisualStudioSKUPath%" == "" if exist "%VisualStudioVersionPath%\Community" set VisualStudioSKUPath=%VisualStudioVersionPath%\Community
 
 rem Add path to MSBuild Binaries
-set PATH=%VisualStudioSKU%\MSBuild\15.0\Bin;%PATH%
+set PATH=%VisualStudioSKUPath%\MSBuild\15.0\Bin;%PATH%
 
 rem Add path to Visual Studio
-set PATH=%VisualStudioSKU%\Common7\IDE;%PATH%
+set PATH=%VisualStudioSKUPath%\Common7\IDE;%PATH%
