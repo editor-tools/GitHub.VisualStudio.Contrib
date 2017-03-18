d91f5d4f323a10149b7e34cdca763e3bf5fd29da
Jamie Cansdale <jcansdale@gmail.com>
2017-03-18 10:48:30 +0000

    Get IViewHost from IGitHubToolWindowManager
    Because `IGitHubToolWindowManager` is in the `GitHub.VisualStudio` assembly, simply
    calling the `ShowHomePane` method requires all kinds of assembly resolving hijinks.

---
+++ src/GitHub.VisualStudio.Contrib/Console/ToolWindowManagerUtilities.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using GitHub.Services;
+using GitHub.ViewModels;
+
+namespace GitHub.VisualStudio.Contrib.Console
+{
+    class ToolWindowManagerUtilities
+    {
+        internal static IViewHost ShowHomePane(IGitHubServiceProvider gitHubServiceProvider)
+        {
+            using (new AssemblyResolver(gitHubServiceProvider.GetType()))
+            {
+                return ResolvingShowHomePane(gitHubServiceProvider);
+            }
+        }
+
+        static IViewHost ResolvingShowHomePane(IGitHubServiceProvider gitHubServiceProvider)
+        {
+            var windowManager = gitHubServiceProvider.GetService<IGitHubToolWindowManager>();
+            return windowManager.ShowHomePane();
+        }
+
+        class AssemblyResolver : IDisposable
+        {
+            string dir;
+
+            internal AssemblyResolver(Type type)
+            {
+                dir = Path.GetDirectoryName(type.Assembly.Location);
+                AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            }
+
+            Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+            {
+                var name = new AssemblyName(args.Name).Name;
+                var file = Path.Combine(dir, name + ".dll");
+                if (File.Exists(file))
+                {
+                    Trace.WriteLine("Resolving: " + file);
+                    return Assembly.LoadFrom(file);
+                }
+
+                return null;
+            }
+
+            public void Dispose()
+            {
+                AppDomain.CurrentDomain.AssemblyResolve -= CurrentDomain_AssemblyResolve;
+            }
+        }
+    }
+}
---
+++ src/GitHub.VisualStudio.Contrib/Console/WindowManagerSubcommands.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell;
+using GitHub.UI;
+using GitHub.Services;
+
+namespace GitHub.VisualStudio.Contrib.Console
+{
+    public partial class WindowManagerSubcommands
+    {
+        IConsoleContext consoleContext;
+        IGitHubServiceProvider gitHubServiceProvider;
+
+        [ImportingConstructor]
+        public WindowManagerSubcommands(IConsoleContext consoleContext, IGitHubServiceProvider gitHubServiceProvider)
+        {
+            this.consoleContext = consoleContext;
+            this.gitHubServiceProvider = gitHubServiceProvider;
+        }
+
+        [Export, SubcommandMetadata("Clone")]
+        public void Clone()
+        {
+            var homePane = ToolWindowManagerUtilities.ShowHomePane(gitHubServiceProvider);
+            var viewWithData = new ViewWithData(UIControllerFlow.Clone);
+            homePane.ShowView(viewWithData);
+        }
+
+        [Export, SubcommandMetadata("Publish")]
+        public void Publish()
+        {
+            var homePane = ToolWindowManagerUtilities.ShowHomePane(gitHubServiceProvider);
+            var viewWithData = new ViewWithData(UIControllerFlow.Publish);
+            homePane.ShowView(viewWithData);
+        }
+    }
+}
