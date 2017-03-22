0da60b96495ea37a18857bff2d45f7c8adf0db09
Jamie Cansdale <jcansdale@gmail.com>
2017-03-22 16:02:10 +0000

    Add assembly resolver for redirecting assembly versions
    Assemblies loaded using `Assembly.LoadFrom` from need to be redirected using an assembly resolver (binding redirects would put them in the `Assembly.Load` context).

---
+++ src/GitHub.VisualStudio.Contrib/AssemblyResolver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.ComponentModel.Composition;
+using System.Collections.Generic;
+
+namespace GitHub.VisualStudio.Contrib
+{
+    [Export]
+    class AssemblyResolver : IDisposable
+    {
+        internal AssemblyResolver(Type type, params string[] preloadFileNames)
+        {
+            BaseDirectory = Path.GetDirectoryName(type.Assembly.Location);
+            ResolvedAssemblies = new Dictionary<ResolveEventArgs, Assembly>();
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            foreach(var fileName in preloadFileNames)
+            {
+                var file = Path.Combine(BaseDirectory, fileName);
+                var assembly = Assembly.LoadFrom(file);
+                Trace.WriteLine($"Loaded '{assembly}' from '{file}'.");
+            }
+        }
+
+        public void Dispose()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve -= CurrentDomain_AssemblyResolve;
+        }
+
+        internal string BaseDirectory { get; }
+        internal IDictionary<ResolveEventArgs, Assembly> ResolvedAssemblies { get; }
+
+        Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            // NOTE: This is currently resolving too much!
+            var name = new AssemblyName(args.Name).Name;
+            var file = Path.Combine(BaseDirectory, name + ".dll");
+            if (File.Exists(file))
+            {
+                Trace.WriteLine($"Resolving '{args.Name}' to '{file}'.");
+                var assembly = Assembly.LoadFrom(file);
+                ResolvedAssemblies[args] = assembly;
+                return assembly;
+            }
+
+            return null;
+        }
+    }
+}
